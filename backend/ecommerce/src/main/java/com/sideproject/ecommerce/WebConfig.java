d710cba89e4935e4533062e150bd5098f7b1d28b
alan <alan@Ecommerce.myguest.virtualbox.org>
2025-04-08 13:12:09 +0800

    feat(login): update CORS rule to allow requests form local env

--- backend/ecommerce/src/main/java/com/sideproject/ecommerce/WebConfig.java
+++ backend/ecommerce/src/main/java/com/sideproject/ecommerce/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 允許所有路徑
-                .allowedOrigins("http://localhost:5173") // 允許的來源
+                .allowedOriginPatterns("http://localhost:*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 允許的 HTTP 方法
                 .allowCredentials(true); // 允許憑證
     }
