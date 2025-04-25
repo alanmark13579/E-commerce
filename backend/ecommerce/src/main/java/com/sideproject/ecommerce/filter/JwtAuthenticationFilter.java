17f5e1031ae2f2b9e5dd620d1a4ce57785b7fe6a
alan <alan@Ecommerce.myguest.virtualbox.org>
2025-04-25 11:33:25 +0800

    feat(jwt): add jwt filter

---
+++ backend/ecommerce/src/main/java/com/sideproject/ecommerce/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,87 @@
+package com.sideproject.ecommerce.filter;
+
+import com.sideproject.ecommerce.model.UserPrincipal;
+import com.sideproject.ecommerce.service.UserService;
+import com.sideproject.ecommerce.util.JwtUtil;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtUtil jwtUtil;
+    private final UserService userService;
+
+    @Autowired
+    public JwtAuthenticationFilter(JwtUtil jwtUtil, UserService userService) {
+        this.jwtUtil = jwtUtil;
+        this.userService = userService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        if (request.getMethod().equalsIgnoreCase("OPTIONS")) {
+            response.setStatus(HttpServletResponse.SC_OK);
+            return;
+        }
+
+        String requestPath = request.getServletPath();
+
+        // Don't validate Public paths
+        List<String> publicPaths = Arrays.asList(
+                "/login",
+                "/register"
+        );
+
+        if (publicPaths.contains(requestPath)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String authorizationHeader = request.getHeader("Authorization");
+
+        // If Authorization Header doesn't exist or format error, return 401
+        if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.getWriter().write("Unauthorized: Missing or invalid Authorization header");
+            return;
+        }
+
+        String token = authorizationHeader.substring(7);
+        String username = jwtUtil.extractUsername(token);
+
+        // If Jwt token can't validate username, return 401
+        if (username == null || SecurityContextHolder.getContext().getAuthentication() != null) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.getWriter().write("Unauthorized: Invalid token");
+            return;
+        }
+
+        // Validate Jwt Token and set user role
+        UserPrincipal userDetails = userService.loadUserByUsername(username);
+        if (jwtUtil.validateToken(token, userDetails)) {
+            UsernamePasswordAuthenticationToken authenticationToken =
+                    new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+        } else {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.getWriter().write("Unauthorized: Token validation failed");
+            return;
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
--- backend/ecommerce/src/main/java/com/sideproject/ecommerce/service/UserService.java
+++ backend/ecommerce/src/main/java/com/sideproject/ecommerce/service/UserService.java
@@ -1,21 +1,27 @@
 package com.sideproject.ecommerce.service;
 
 import com.sideproject.ecommerce.model.User;
+import com.sideproject.ecommerce.model.UserPrincipal;
 import com.sideproject.ecommerce.repository.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
     private final UsersRepository usersRepository;
 
     @Autowired
-    public  UserService(UsersRepository usersRepository) {
-        this.usersRepository = usersRepository;
-    }
+    public  UserService(UsersRepository usersRepository) {this.usersRepository = usersRepository;}
 
-    public User getUserByEmail(String email) {
-        return usersRepository.findByEmail(email);
+    public User getUserByEmail(String email) {return usersRepository.findByEmail(email);}
+
+    @Override
+    public UserPrincipal loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = usersRepository.findByName(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return new UserPrincipal(user);
     }
 }
